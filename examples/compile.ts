7ee3cc19e495fd33f2e6a1d63630bb70853cfa14
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-20 13:00:09 +0530

    feat: use options in example

--- examples/compile.ts
+++ examples/compile.ts
@@ -2,10 +2,14 @@ import { compile } from "../mod.ts";
 
 console.log(
   compile(`
-$bg: #eee;
-
-body {
-  color: $bg;
-}
-`).result,
+    $bg: #eee;
+    body {
+      color: $bg;
+    }
+  `, {
+    output_style: "compressed",
+    precision: 5,
+    indented_syntax: false,
+    include_paths: []
+  }).result,
 );
