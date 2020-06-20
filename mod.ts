4942353427b2a7b47330b9b766246ea101a00a53
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-20 12:57:46 +0530

    feat: expose opts to compile

--- mod.ts
+++ mod.ts
@@ -2,6 +2,8 @@ import {
   compile as rs_compile,
 } from "./plugin/index.ts";
 
-export function compile(str: string) {
-  return rs_compile(str);
+import { IOptions } from "./types/options.ts";
+
+export function compile(str: string, opts?: IOptions) {
+  return rs_compile(str, opts);
 }
