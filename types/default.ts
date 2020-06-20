72e63ca1bba6b77165abbff27144bdf38654d1eb
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-20 12:56:05 +0530

    fix: typos

--- types/default.ts
+++ types/default.ts
@@ -1,6 +1,6 @@
 import { IOptions } from "./options.ts";
 
-export const defaultOptions: IOptions {
+export const defaultOptions: IOptions = {
   output_style: "nested",
   precision: 5,
   indented_syntax: false,
