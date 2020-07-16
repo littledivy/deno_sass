db8c0e8c4989ec6bb1048c889389562913614a36
divy-work <dj.srivastava23@gmail.com>
2020-07-16 15:47:57 +0530

    feat: add compile file op

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ mod utils;
 #[no_mangle]
 pub fn deno_plugin_init(interface: &mut dyn Interface) {
     interface.register_op("compile_str", op_compile);
+    interface.register_op("compile_file", op_compile_file);
 }
 
 #[derive(Deserialize)]
@@ -36,7 +37,7 @@ fn op_compile(_interface: &mut dyn Interface, data: &[u8], _zero_copy: &mut [Zer
         include_paths: params.include_paths,
         indented_syntax: params.indented_syntax,
     };
-    let mut response: CompileResponse;
+    let response: CompileResponse;
     match sass_rs::compile_string(&params.content, opt) {
         Ok(r) => {
             response = CompileResponse {
@@ -55,3 +56,30 @@ fn op_compile(_interface: &mut dyn Interface, data: &[u8], _zero_copy: &mut [Zer
     Op::Sync(result_box)
 }
 
+
+fn op_compile_file(_interface: &mut dyn Interface, data: &[u8], _zero_copy: &mut [ZeroCopyBuf]) -> Op {
+    let params: CompileArguments = serde_json::from_slice(data).unwrap();
+    let opt = sass_rs::Options {
+        output_style: utils::str_to_style(&params.output_style),
+        precision: params.precision,
+        include_paths: params.include_paths,
+        indented_syntax: params.indented_syntax,
+    };
+    let response: CompileResponse;
+    match sass_rs::compile_file(&params.content, opt) {
+        Ok(r) => {
+            response = CompileResponse {
+                result: Some(r),
+                error: None,
+            };
+        }
+        Err(e) => {
+            response = CompileResponse {
+                result: None,
+                error: Some(e.to_string()),
+            };
+        }
+    }
+    let result_box: Buf = serde_json::to_vec(&response).unwrap().into_boxed_slice();
+    Op::Sync(result_box)
+}
