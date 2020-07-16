64fa514c45d01f3becca79bdbebac4d7ffb6467c
divy-work <dj.srivastava23@gmail.com>
2020-07-16 15:51:26 +0530

    fet: expose compile_file func

--- mod.ts
+++ mod.ts
@@ -1,9 +1,14 @@
 import {
   compile as rs_compile,
+  compile_from_file as rs_compile_file
 } from "./plugin/index.ts";
 
 import { IOptions } from "./types/options.ts";
 
 export function compile(str: string, opts?: IOptions) {
   return rs_compile(str, opts);
+}
+
+export function compile_file(file: string, opts?: IOptions) {
+  return rs_compile_file(file, opts);
 }
